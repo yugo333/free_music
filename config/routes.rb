b21f3390f964f912b3aa32fd3b70b3c92be5190c
yugo333 <yugo199103@gmail.com>
2019-12-21 16:29:47 +0900

    contours1/2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "musics#show"
-  resources :musics, only: [:index]
+
+  root to: "fronts#index"
+  resources :musics, only: [:index,:new,:create,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
